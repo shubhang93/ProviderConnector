8d62c4e6f0220bee3f53ff777ff5d6a924e60792
shubhang93 <shubhang.sabbath@gmail.com>
2017-07-02 12:23:42 +0530

    delete unwanted resources

--- src/Provider/Provider.js
+++ src/Provider/Provider.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 class Provider extends Component {
   getChildContext() {
-    return this.props;
+    return this.props.contextStore;
   }
 
   render() {
     return React.Children.only(this.props.children);
   }
+}
+
+Provider.childContextTypes = {
+    contextStore: PropTypes.object
+}
+
+Provider.propTypes = {
+    contextStore:PropTypes.object.isRequired
 }
 
 export default Provider;
